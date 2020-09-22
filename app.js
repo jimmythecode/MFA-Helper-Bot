43f50bb8cb6c102c1a7426c4a0e88669fdeecf4f
jimmythecode <jimmythecode@gmail.com>
2020-09-22 16:40:22 +0100

    think this is ready for heroku - just checking on test subreddit

--- app.js
+++ app.js
@@ -16,28 +16,25 @@ const r = new snoowrap({
 const stream = new CommentStream(r, {
     subreddit: "testingground4bots",
     limit: 1,
-    pollTime: 6000
+    pollTime: 1000
 });
 
 stream.on("item", comment => {
-    console.log("running the stream");
     console.log("node env: ", process.env.NODE_ENV);
-    console.log('current time: ',dt.toLocaleString(DateTime.DATETIME_MED));
-    console.log(`got-shirt  ...n //n \n Hello
-    also hello`);
+    let currentDate = new Date()
+    let dateFormat = new Date(comment.created_utc * 1000)
+    // console.log(dateFormat, ();
+    let isAuthorTrue = comment.author.name === 'JimmyTheCode'
+    let terms = ['t-shirt', 'fit']
+    let doesBodyInclude = terms.every(term => comment.body.includes(term))
+    let isWithinAnHour = (currentDate - dateFormat) / 1000 / 60 / 60 < 1
 
-    if (comment.author.name === 'JimmyTheCode') {
- 
-        // writeReqObjectToFile(comment, 'comment3.json')
-        if (comment.body === 't-shirt') {
-            try {
-                comment.reply(`this is a bot response
-                about something.
-                [now we're trying a link](https://www.reddit.com/comments/ih2kag)
-                ${dt.toLocaleString(DateTime.DATETIME_MED)}`)
-            } catch (error) {
-                console.error('comment.reply() attempt did not work', error);
-            }
+    console.log(isAuthorTrue, doesBodyInclude, isWithinAnHour);
+    if (isAuthorTrue && doesBodyInclude && isWithinAnHour) {
+        try {
+            comment.reply(`Hi, this an automatic response because you said these words: [${terms}]\n\nThese links might be useful from the [MFA Wiki](https://www.reddit.com/r/malefashionadvice/wiki/itemguides#wiki_clothing):\n\n*  [A Comprehensive Guide To Basic White T-Shirts](https://www.reddit.com/r/malefashionadvice/comments/clo2um/a_comprehensive_guide_to_basic_white_tshirts/)\n\n*  [Your Favorite ___ for $___: Tee Shirts](https://www.reddit.com/r/malefashionadvice/comments/f9vrkb/your_favorite_for_tee_shirts/)\n\nThis post was sent at: ${dt.toLocaleString(DateTime.DATETIME_MED)} From: ${process.env.NODE_ENV}`)
+        } catch (error) {
+            console.error('comment.reply() attempt did not work', error);
         }
     }
 })
